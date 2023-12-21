89c362b2815717325dd54936e83ae1bc6793bb20
F2419105 Diego De Amorim Rocha Peixoto <diego.peixoto@bb.com.br>
2023-12-21 14:29:03 -0300

    refact: add templates

--- README.md
+++ README.md
@@ -4,10 +4,28 @@
 
 pip install Django
 
-django-admin help -> Exibe todos os comandos que podem ser utilizados com django
+`django-admin help` -> Exibe todos os comandos que podem ser utilizados com django
+
+`python manage.py help` -> Exibe os comandos que podemos rodar com o manage
+
+- autenticação
+- senhas
+- banco de dados
+- app (funcionalidades da aplicação)
 
 Iniciar um projeto
-django-admin statrproject setup .
+`django-admin statrproject setup .`
 
 Subindo o servidor
-python manage.py runserver
+
+`python manage.py runserver`
+
+Iniciar um app
+
+`python manage.py startapp galeria`
+
+alterar o **setup/settings.py** incluindo o app criado na lista de **INSTALLED_APPS**
+
+## Dicas de boas práticas
+
+1. Cada app deve ter sua própria lista de urls, evitando assim popular a lista de urls de setup
--- setup/settings.py
+++ setup/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'galeria',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Pasta onde os nossos arquivos estáticos estão salvos
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'setup/static')
+]
+
+# Caminho absoluto do diretório onde o python vai coletar os arquivos estaticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
